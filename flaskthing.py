2d478164c51a29842ea72add2ddd51a0a029517d
s-brian <shih.brian123@gmail.com>
2022-10-16 04:18:40 -0700

    more flask edits

--- flaskthing.py
+++ flaskthing.py
@@ -1,14 +1,51 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from functions import *
 
-
 app = Flask(__name__)
+df = pd.read_csv("Food_Product_Emissions.csv")
 
-@app.route("/")
+
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        ingredients = {}
+        #sets ingredient to the input value from textbox
+        ingredient = request.form["userinput"]
+        #adds the ingredient to the dictionary so it can be a parameter for get_ingredients
+        ingredients[ingredient] = ""
+        ingredients = convert_dictionary(ingredients)
+        found_ingredients = get_ingredients(ingredients, df)
+        emissions = get_emissions(found_ingredients)
+        total_emissions = get_total_emissions(emissions)
+
+
+        #working on trying to pass other info (feed, farm, processing, transport, etc) into index.html
+        ingredient_list = dictionary_to_list(ingredient)
+        target_ingredients = get_ingredients(ingredient_list, df)
+        more_info = []
+        for x in target_ingredients:
+            more_info.append(x)
+
+
+                                            #this is where you pass the parameters into index.html
+        return render_template("index.html", total_emissions=total_emissions, more_info=target_ingredients)
+
+    else:
+        return render_template("index.html")
+
+@app.route("/<test>")
+def user(test):
+    return f"<h1>{test}</h1>"
 
 
 
 if __name__ == ("__main__"):
     app.run(debug=True)
+
+
+    app.run()
+    # ingredients = {"Apples": ""}
+    # target_ingredients = get_ingredients(ingredients, df)
+    # print(target_ingredients)
+    # {"Apples, Bananas": }
+
--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-# return only target ingrediants as a df
+# return only target ingredients as a df #param list, csv returns dataframe of only ingredient
 def get_ingredients(ingredients, df):
 
     target_ingredients = pd.DataFrame(columns=df.columns)
@@ -14,13 +14,13 @@ def get_ingredients(ingredients, df):
 
     return target_ingredients
 
-# return ingredient emissions as a dict
+# return ingredient emissions as a dict #takes data frame returns dictionary values are emissions
 def get_emissions(target_ingredients):
 
     ingredients_emissions = target_ingredients.set_index('Food product').to_dict()['Total from Land to Retail']
     return ingredients_emissions
 
-# get total emissions of meal
+# get total emissions of meal #returns float input is dictionary
 def get_total_emissions(ingredients_emissions):
 
     print(type(ingredients_emissions))
@@ -28,7 +28,7 @@ def get_total_emissions(ingredients_emissions):
     total_emissions = sum(ingredients_emissions.values())
     return total_emissions
 
-# get percent of total emissions of each ingredient as a dict
+# get percent of total emissions of each ingredient as a dict #param dictionary returns percentage
 def get_percentages(ingredients_emissions, total_emissions):
 
     percent_total_emissions = {}
@@ -38,13 +38,46 @@ def get_percentages(ingredients_emissions, total_emissions):
 
     return percent_total_emissions
 
+
+#the user input returns a single dictionary key (ex: {"Bananas, Apple"}), so this converts it into {"Bananas", "Apples"}
+def convert_dictionary(dictionary):
+    list = []
+    done_dictionary = {}
+    for x in dictionary:
+        list.append(x)
+    splitlist = list[0].split(",")
+    count = 0
+    for x in splitlist:
+        splitlist[count] = x.strip();
+        count += 1
+    for x in splitlist:
+        print(x)
+        done_dictionary[x] = ""
+    return(done_dictionary)
+
+def dictionary_to_list(dictionary):
+    list = []
+    for x in dictionary:
+        list.append(x)
+    splitlist = list[0].split(",")
+    count = 0
+    for x in splitlist:
+        splitlist[count] = x.strip();
+        count += 1
+    return(splitlist)
+
+
+
+
+
+
 def main():
 
     # import data into df
-    df = pd.read_csv("/Users/claire/Documents/Projects/recipe_emissions/Food_Product_Emissions.csv")
+    df = pd.read_csv("Food_Product_Emissions.csv")
 
     # get specific ingredients
-    ingredients = {"Apples", "Bananas", "Palm Oil", "Tofu", "test"}
+    ingredients = ("Apples", "Bananas", "Palm Oil", "Tofu", "test")
     target_ingredients = get_ingredients(ingredients, df)
 
     # get specific ingredients emissions
@@ -58,6 +91,10 @@ def main():
     # get percent total emissions for each ingredient
     percent_total_emissions = get_percentages(ingredients_emissions, total_emissions)
     print(percent_total_emissions)
+
+    testdict = {"Apples, Bananas,Mango"}
+    print(target_ingredients)
+    print(dictionary_to_list(testdict))
 
 if __name__ == '__main__':
     main()
